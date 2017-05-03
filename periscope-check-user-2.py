7e237c330156a1c672d58f62a5079d47dd194fac
ru <git@xoyo.ru>
2017-05-03 13:51:03 +0200

    Update periscope-check-user-2.py

--- periscope-check-user-2.py
+++ periscope-check-user-2.py
@@ -4,12 +4,15 @@ import urllib.request, urllib.error
 import sys, csv, time
 from multiprocessing.dummy import Pool as ThreadPool
 from threading import Lock
+from pyperi import Peri
 
-if len(sys.argv) < 2:
+pp = Peri()
+
+if len(sys.argv) != 2:
     print("Usage: %s /path/to/users.csv" % (str(sys.argv[0])))
     sys.exit(1)
 
-filecsv=sys.argv[1] 
+filecsv_file = sys.argv[1]
 
 PERISCOPE_URL = 'https://www.periscope.tv/'
 usernames = []
@@ -18,11 +21,15 @@ def attempt(unames):
     try:
         conn = urllib.request.urlopen(PERISCOPE_URL+unames[:-2])
     except urllib.error.HTTPError as e:
-    	print("[-] " + unames[:-2] + " " + str(e.code))
+        if e.code == 408:
+            return
+        else:
+            pass
+        print("[ + ] " + unames[:-2] + " " + str(e.code))
     else:
-        print("[+] " + unames[:-2] + " " + str(conn.getcode()))
+        print("[ - ] " + unames[:-2] + " " + str(conn.getcode()))
 
-with open(filecsv, "r") as f:
+with open(filecsv_file, "r") as f:
     reader = csv.reader(f, delimiter=',')
     usernames2 = list(reader)
     unames = usernames2[0]
