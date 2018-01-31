12837a15242f7c55fe34de0bdeae5ed444b6978c
ru <git@xoyo.ru>
2018-01-31 22:10:57 +0300

    add headers get script

---
+++ head-server.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# apt-get install python3-dev libxml2-dev libxslt1-dev zlib1g-dev
+import requests as req
+import sys
+from multiprocessing.dummy import Pool as ThreadPool
+from threading import Lock
+import urllib.request, urllib.error
+
+
+ip = [line.strip() for line in open(sys.argv[1], 'r')]
+port = sys.argv[2]
+
+def parse(ip):
+    try:
+        resp = req.head("http://" + str(ip) + ":" + str(port), verify=False, timeout=3)
+        print(str(ip) + " Server: " + resp.headers['server'])
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            return
+        else:
+            return
+    except:
+        return
+
+lock = Lock()
+pool = ThreadPool(20)
+
+pool.map(parse, ip)
+pool.close()
+pool.join()
