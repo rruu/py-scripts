d5716f24209c0e86d3c57c04a8aaf17e37b0990d
ru <git@xoyo.ru>
2018-03-10 00:14:18 +0300

    dbinsert.py change

--- dbinsert.py
+++ dbinsert.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python3
+# require
+# pip3 install psycopg2-binary
+
 import psycopg2
 import sys
+
+#check args
+if len(sys.argv) < 2 or len(sys.argv) > 2:
+    print("Usage: %s data.txt" % (str(sys.argv[0])))
+    sys.exit(1)
 
 #db config
 host = "xxx"
@@ -8,6 +17,9 @@ user = "xxx"
 password = "xxx"
 
 conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}'".format(dbname,user,host,password))
+
+f = [line.strip() for line in open(sys.argv[1], 'r')]
+f = [x for x in f if x]
 
 data = []
 
@@ -27,17 +39,15 @@ except:
 
 try:
     cur = conn.cursor()
-    with open(sys.argv[1]) as f:
-        for line in f:
-            ipaddr = line.split(",")[0]
-            port = line.split(",")[1]
-            header = str(line.split(",")[2]).strip().replace("'", "")
-            #print('{} {} {}'.format(ipaddr,port,header).rstrip())
-            insert_query = "INSERT INTO headers (ipaddr,port,header) values ('{}','{}','{}');\r".format(ipaddr,port,header).strip()
-            data.append(insert_query.strip())
+    for line in f:
+        ipaddr = line.split(",")[0]
+        port = line.split(",")[1]
+        header = str(line.split(",")[2]).strip().replace("'", "")
+        insert_query = "INSERT INTO headers (ipaddr,port,header) values ('{}','{}','{}');\r".format(ipaddr,port,header).strip()
+        data.append(insert_query.strip())
+        print("INSERT COMPLETE")
 except:
-    print("INSERT PROBLEM")
+    print("INSERT ERR")
 #print(''.join(map(str, data)))
 cur.execute(''.join(map(str, data)))
 conn.commit()
-
