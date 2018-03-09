6ff649e08360f80169bf4d3f168fd152ee4924d0
ru <git@xoyo.ru>
2018-03-09 22:11:07 +0300

    change snapshot.py

--- snapshot.py
+++ snapshot.py
@@ -1,54 +1,52 @@
 # -*- coding: utf-8 -*-
-
 import sys
 from multiprocessing.dummy import Pool as ThreadPool
 from threading import Lock
 import urllib.request, urllib.error
-#from datetime import datetime
 import socket
+import re
 
-socket.setdefaulttimeout(20)
+socket.setdefaulttimeout(30)
 
 urls = [line.strip() for line in open(sys.argv[1], 'r')]
+urls = [x for x in urls if x]
+ipPattern = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+login = "admin"
 
 def parse(urls):
     try:
         url = str(urls[7:-1])
-        login = url.split(':')[0]
-        passw = url.split(':')[1]
-        passw = passw.split('@')[0]
+        ipaddr = re.findall(ipPattern,url)
+        passw = str(url.split(':')[1].split('@')[:-1])[2:-2]
+        if passw == []:
+            passw = ''
+        else:
+            pass
         snappath = url.split('@')[1]
         port = url.split(':')[2]
-        #dtnow = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
         filename = snappath.split(':')[0]
-        #filename = str(filename + "-" + passw + ".jpg")
-        filename = str(filename + "-" + passw + "-" + str(port) + ".jpg")
-        #print(url)
-        urllib.request.urlretrieve("http://" + str(snappath) + "/snapshot.cgi?user=" + str(login) + "&pwd=" + str(passw), filename)
+        filename = "{}-{}-{}.jpg".format(filename,passw,port)
+        print("http://{}/snapshot.cgi?user={}&pwd={}".format(snappath,login,passw))
+        urllib.request.urlretrieve("http://{}/snapshot.cgi?user={}&pwd={}".format(snappath,login,passw),filename)
+        
     except urllib.error.HTTPError as e:
         if e.code == 401:
-            print(str(urls) + " err Unauthorized ")
-            #urllib.request.urlopen("http://" + str(snappath) + "/reboot.cgi?=user=" + str(login) + "&pwd=" + str(passw))
+            print("{} err Unauthorized".format(urls))
         else:
             print(str(urls) + " err req. code")
-            #urllib.request.urlopen("http://" + str(snappath) + "/reboot.cgi?&user=" + str(login) + "&pwd=" + str(passw))
-
     except socket.error:
         try:
-            urllib.request.urlopen("http://" + str(snappath) + "/camera_control.cgi?user=" + str(login) + "&pwd=" + str(passw))
-        #print("http://" + str(snappath) + "/snapshot.cgi?user=" + str(login) + "&pwd=" + str(passw) + " err socket ")
+            urllib.request.urlopen("http://{}/camera_control.cgi?user={}&pwd={}".format(snappath,login,passw))
         except urllib.error.HTTPError as e:
             if e.code == 200:
-                urllib.request.urlretrieve("http://" + str(snappath) + "/snapshot.cgi?user=" + str(login) + "&pwd=" + str(passw), filename)
-                print(str("http://" + str(snappath) + "/snapshot.cgi?user=" + str(login) + "&pwd=" + str(passw)) + " camera err ")
+                urllib.request.urlretrieve("http://{}/snapshot.cgi?user={}&pwd={}".format(snappath,login,passw),filename)
+                print("http://{}/snapshot.cgi?user={}&pwd={} camera err".format(snappath,login,passw))
             else:
-                print("http://" + str(snappath) + "/snapshot.cgi?user=" + str(login) + "&pwd=" + str(passw) + " err socket ")
+                print("http://{}/snapshot.cgi?user={}&pwd={} err socket".format(snappath,login,passw))
         except:
-            print(str("http://" + str(snappath) + "/snapshot.cgi?user=" + str(login) + "&pwd=" + str(passw)) + " camera socket err ")
-
+            print("http://{}/snapshot.cgi?user={}&pwd={} camera socket err".format(snappath,login,passw))
     except:
-        print("http://" + str(snappath) + "/snapshot.cgi?user=" + str(login) + "&pwd=" + str(passw) + " err oth ")
-        #urllib.request.urlopen("http://" + str(snappath) + "/reboot.cgi?&user=" + str(login) + "&pwd=" + str(passw))
+        print("http://{}/snapshot.cgi?user={}&pwd={} err oth".format(snappath,login,passw))
 
 lock = Lock()
 pool = ThreadPool(5)
